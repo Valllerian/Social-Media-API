aff69773909051875b3b53d5ee3dd70f42fa5bf7
Valerii Bihun <valeriibihun.co@gmail.com>
2022-01-20 21:45:20 -0700

    Thought Controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,63 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // Get all thoughts:
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a thought:
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID found' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a thought:
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: thought._id.toString() } },
+          { new: true }
+        ).then((userThoughts) => {
+            res.json(userThoughts);
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Delete a thought:
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID found' })
+          : User.deleteMany({ _id: { $in: thought.user } })
+      )
+      .then(() => res.json({ message: 'Selected thought is deleted' }))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Update a course
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with this ID found' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
