ac57a2039fc4cddaa764480a6ddc43763d95ba89
Valerii Bihun <valeriibihun.co@gmail.com>
2022-01-20 21:21:44 -0700

    Thought model update

--- models/Thought.js
+++ models/Thought.js
@@ -1,31 +1,24 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require("./Reaction");
 
-// Schema to create a course model
-const courseSchema = new Schema(
+
+const thoughtSchema = new Schema(
   {
-    courseName: {
+    thoughtText: {
       type: String,
       required: true,
+      maxlength: 280,
+      minlength: 1,
     },
-    inPerson: {
-      type: Boolean,
-      default: true,
-    },
-    startDate: {
+    createdAt: {
       type: Date,
       default: Date.now(),
     },
-    endDate: {
-      type: Date,
-      // Sets a default value of 12 weeks from now
-      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    username: {
+      type: String,
+      required: true,
     },
-    students: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Student',
-      },
-    ],
+    reactions: [reactionSchema]
   },
   {
     toJSON: {
@@ -35,6 +28,6 @@ const courseSchema = new Schema(
   }
 );
 
-const Course = model('course', courseSchema);
+const Thought = model('thought', thoughtSchema);
 
-module.exports = Course;
+module.exports = Thought;
