6594e2efb209c5cf6b4906e853747dcff619a488
Valerii Bihun <valeriibihun.co@gmail.com>
2022-01-21 12:41:49 -0700

    working on routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => res.send('Wrong route!'));
+router.use((req, res) => res.send('Check your routes!'));
 
 module.exports = router;
